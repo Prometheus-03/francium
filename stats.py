8dc066cbc185fb40b9afe0127f5b0f5026d63381
Prometheus <33342399+SVengat03@users.noreply.github.com>
2020-07-15 22:49:45 +0800

    Add files via upload

--- stats.py
+++ stats.py
@@ -4,31 +4,40 @@ from discord.ext import commands
 import discord
 from dbwrapper import DB
 
+
 class Stats(commands.Cog):
-    def __init__(self,bot):
-        self.bot=bot
+    def __init__(self, bot):
+        self.bot = bot
 
     @commands.command()
-    async def daily(self,ctx):
+    async def daily(self, ctx):
         '''Get your daily rewards'''
         data = DB("users")
+        newdaily = True
         data.set_collection("currency")
-        match = await data.find(userid=ctx.author.id)
-        if match:
-            user = match[0]
-            if 'cooldown' not in user.keys():
-                user["money"] = 500
-            elif int(datetime.datetime.now().timestamp()) - user['cooldown']>=86400:
-                user["money"] += 500
+        async with ctx.typing():
+            match = await data.find(userid=ctx.author.id)
+            if match:
+                user = match[0]
+                if 'cooldown' not in user.keys():
+                    user["money"] = 500
+                    user['cooldown'] = int(datetime.datetime.now().timestamp())
+                elif int(datetime.datetime.now().timestamp()) - user['cooldown'] >= 86400:
+                    user["money"] += 500
+                    user['cooldown'] = int(datetime.datetime.now().timestamp())
+                else:
+                    print(int(datetime.datetime.now().timestamp()),user['cooldown'])
+                    conversion = datetime.timedelta(
+                        seconds=(user['cooldown'] + 86400 - int(datetime.datetime.now().timestamp())))
+                    converted_time = str(conversion)
+                    await ctx.send(f"{ctx.author.mention}, you can collect your dailies in {converted_time}.")
+                    newdaily = False
+                await data.update(user["_id"], money=user['money'], cooldown=user['cooldown'])
             else:
-                conversion = datetime.timedelta(seconds=user['cooldown'])
-                converted_time = str(conversion)
-                await ctx.send(f"{ctx.author.mention}, you can collect your dailies in {converted_time}.")
-            user['cooldown'] = int(datetime.datetime.now().timestamp())
-            await data.update(user["_id"],**user)
-        else:
-            await data.insertnorepeat(userid=ctx.author.id,money=500,cooldown=int(datetime.datetime.now().timestamp()))
-        await ctx.send(f"{ctx.author.mention}, you have collected your dailies!")
+                await data.insertnorepeat(userid=ctx.author.id, money=500,
+                                          cooldown=int(datetime.datetime.now().timestamp()))
+        if newdaily:
+            await ctx.send(f"{ctx.author.mention}, you have collected your daily bonus of 500 silver!")
 
 
 def setup(bot):
